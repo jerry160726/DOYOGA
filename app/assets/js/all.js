6a8f170c9cac0f9f95cbfbc21ee8da54eb1bc472
Jerry Yen <jerry160726@gmail.com>
2021-09-01 22:49:57 +0800

    first commit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,10 @@
-(function() {
+(function () {
 
   'use strict';
 
   // breakpoint where swiper will be destroyed
   // and switches to a dual-column layout
-  const breakpoint = window.matchMedia( '(max-width: 992px)' );
+  const breakpoint = window.matchMedia('(max-width: 992px)');
 
   // keep track of swiper instances to destroy later
   let mySwiper;
@@ -13,19 +13,19 @@
   //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
 
-  const breakpointChecker = function() {
+  const breakpointChecker = function () {
 
     // if larger viewport and multi-row layout needed
-    if ( breakpoint.matches === true ) {
+    if (breakpoint.matches === true) {
 
       // clean up old instances and inline styles when available
-      if ( mySwiper !== undefined ) mySwiper.destroy( true, true );
+      if (mySwiper !== undefined) mySwiper.destroy(true, true);
 
       // or/and do nothing
       return;
 
       // else if a small viewport and single column layout needed
-    } else if ( breakpoint.matches === false ) {
+    } else if (breakpoint.matches === false) {
 
       // fire small viewport version of swiper
       return enableSwiper();
@@ -38,9 +38,9 @@
   //////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////
 
-  const enableSwiper = function() {
+  const enableSwiper = function () {
 
-    mySwiper = new Swiper ('.swiper-container', {
+    mySwiper = new Swiper('.swiper-container', {
 
       slidesPerView: 1.5,
       spaceBetween: 30,
@@ -62,3 +62,37 @@
 
 
 })(); /* IIFE end */
+
+
+// 聽聽他們怎麼說
+var swiper = new Swiper(".comSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  grid: {
+    rows: 3,
+    fill: 'column',
+  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+      spaceBetween: 30,
+    },
+  },
+});
